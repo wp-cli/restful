281a7063ec00dd9886c7a0ef4b8645d6469a99de
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-22 05:43:36 -0800

    Register remote endpoints as local commands

--- inc/Runner.php
+++ inc/Runner.php
@@ -3,11 +3,43 @@
 namespace WP_REST_CLI;
 
 use WP_CLI;
+use WP_CLI\Utils;
+use WP_REST_Request;
 use WP_REST_Server;
 
 class Runner {
 	
-	public static function before_wp_load() {
+	/**
+	 * When --http=domain.com is passed as global arg, register REST for it
+	 */
+	public static function load_remote_commands() {
+		
+		$global_args = array();
+		foreach( array_slice( $GLOBALS['argv'], 1 ) as $maybe_arg ) {
+			if ( 0 === strpos( $maybe_arg, '--' ) ) {
+				$global_args[] = $maybe_arg;
+			} else {
+				break;
+			}
+		}
+		if ( ! empty( $global_args ) ) {
+			$configurator = WP_CLI::get_configurator();
+			list( $args, $assoc_args, $runtime_config ) = $configurator->parse_args( $global_args );
+			if ( ! empty( $assoc_args['http'] ) ) {
+				$api_url = self::auto_discover_api( $assoc_args['http'] );
+				if ( $api_url ) {
+					$api_index = self::get_api_index( $api_url );
+					if ( $api_index ) {
+						foreach( $api_index['routes'] as $route => $route_data ) {
+							if ( false === stripos( $route, '/wp/v2/comments' ) ) {
+								continue;
+							}
+							self::register_route_commands( $route, $route_data, array( 'when' => 'before_wp_load' ) );
+						}
+					}
+				}
+			}
+		}
 		
 	}
 	
@@ -22,116 +54,168 @@ class Runner {
 
 		$wp_rest_server = new WP_REST_Server;
 		do_action( 'rest_api_init', $wp_rest_server );
-
-		foreach( $wp_rest_server->get_routes() as $route => $endpoints ) {
-
+		
+		$request = new WP_REST_Request( 'GET', '/' );
+		$request->set_param( 'context', 'help' );
+		$response = $wp_rest_server->dispatch( $request );
+		$response_data = $response->get_data();
+		if ( empty( $response_data ) ) {
+			return;
+		}
+		
+		foreach( $response_data['routes'] as $route => $endpoints ) {
 			if ( false === stripos( $route, '/wp/v2/comments' ) ) {
 				continue;
 			}
+			self::register_route_commands( $route, $endpoints );
+		}
+	}
+	
+	/**
+	 * Auto-discover the WP-API index from a given URL
+	 *
+	 * @param string $url
+	 * @return string|false
+	 */
+	private static function auto_discover_api( $url ) {
+		if ( false === stripos( $url, 'http://' ) && false === stripos( $url, 'https://' ) ) {
+			$url = 'http://' . $url;
+		}
+		$response = Utils\http_request( 'HEAD', $url );
+		if ( empty( $response->headers['link'] ) ) {
+			return false;
+		}
+		$bits = explode( ';', $response->headers['link'] );
+		if ( 'rel="https://api.w.org/"' !== trim( $bits[1] ) ) {
+			return false;
+		}
+		return trim( $bits[0], '<>' );
+	}
+	
+	/**
+	 * Get the index data from an API url
+	 *
+	 * @param string $api_url
+	 * @return array|false
+	 */
+	private static function get_api_index( $api_url ) {
+		$query_char = false !== strpos( $api_url, '?' ) ? '&' : '?';
+		$api_url .= $query_char . 'context=help';
+		$response = Utils\http_request( 'GET', $api_url );
+		if ( empty( $response->body ) ) {
+			return false;
+		}
+		return json_decode( $response->body, true ); 
+	}
+	
+	/**
+	 * Register WP-CLI commands for all endpoints on a route
+	 *
+	 * @param string
+	 * @param array $endpoints
+	 */
+	private function register_route_commands( $route, $route_data, $command_args = array() ) {
+		
+		$parent = "rest {$route_data['schema']['title']}";
+		$fields = array();
+		foreach( $route_data['schema']['properties'] as $key => $args ) {
+			if ( in_array( 'embed', $args['context'] ) ) {
+				$fields[] = $key;
+			}
+		}
 
-			$route_data = $wp_rest_server->get_data_for_route( $route, $endpoints, 'help' );
-			$parent = "rest {$route_data['schema']['title']}";
-			$fields = array();
-			foreach( $route_data['schema']['properties'] as $key => $args ) {
-				if ( in_array( 'embed', $args['context'] ) ) {
-					$fields[] = $key;
-				}
+		foreach( $route_data['endpoints'] as $endpoint ) {
+
+			$parsed_args = preg_match_all( '#\([^\)]+\)#', $route, $matches );
+			$resource_id = ! empty( $matches[0] ) ? array_pop( $matches[0] ) : null;
+			$trimmed_route = rtrim( $route );
+			$is_singular = $resource_id === substr( $trimmed_route, - strlen( $resource_id ) );
+
+			$command = $method = '';
+			// List a collection
+			if ( array( 'GET' ) == $endpoint['methods']
+				&& ! $is_singular ) {
+				$command = 'list';
 			}
 
-			foreach( $endpoints as $endpoint ) {
+			// Create a specific resource
+			if ( array( 'POST' ) == $endpoint['methods']
+				&& ! $is_singular ) {
+				$command = 'create';
+			}
 
-				$parsed_args = preg_match_all( '#\([^\)]+\)#', $route, $matches );
-				$resource_id = ! empty( $matches[0] ) ? array_pop( $matches[0] ) : null;
-				$trimmed_route = rtrim( $route );
-				$is_singular = $resource_id === substr( $trimmed_route, - strlen( $resource_id ) );
+			// Get a specific resource
+			if ( array( 'GET' ) == $endpoint['methods']
+				&& $is_singular ) {
+				$command = 'get';
+			}
 
-				$command = $method = '';
-				// List a collection
-				if ( array( 'GET' => true ) == $endpoint['methods']
-					&& ! $is_singular ) {
-					$command = 'list';
-				}
+			// Update a specific resource
+			if ( in_array( 'POST', $endpoint['methods'] )
+				&& $is_singular ) {
+				$command = 'update';
+			}
 
-				// Create a specific resource
-				if ( array( 'POST' => true ) == $endpoint['methods']
-					&& ! $is_singular ) {
-					$command = 'create';
-				}
+			// Delete a specific resource
+			if ( array( 'DELETE' ) == $endpoint['methods']
+				&& $is_singular ) {
+				$command = 'delete';
+			}
 
-				// Get a specific resource
-				if ( array( 'GET' => true ) == $endpoint['methods']
-					&& $is_singular ) {
-					$command = 'get';
-				}
+			if ( empty( $command ) ) {
+				continue;
+			}
 
-				// Update a specific resource
-				if ( array_key_exists( 'POST', $endpoint['methods'] )
-					&& $is_singular ) {
-					$command = 'update';
-				}
+			$rest_command = new RestCommand( $route_data['schema']['title'], $trimmed_route, $resource_id, $fields );
 
-				// Delete a specific resource
-				if ( array( 'DELETE' => true ) == $endpoint['methods']
-					&& $is_singular ) {
-					$command = 'delete';
-				}
-
-				if ( empty( $command ) ) {
-					continue;
-				}
-
-				$rest_command = new RestCommand( $route_data['schema']['title'], $trimmed_route, $resource_id, $fields );
-
-				$synopsis = array();
-				if ( in_array( $command, array( 'delete', 'get', 'update' ) ) ) {
-					$synopsis[] = array(
-						'name'        => 'id',
-						'type'        => 'positional',
-						'description' => 'The id for the resource.',
-						'optional'    => false,
-					);
-				}
-
-				if ( ! empty( $endpoint['args'] ) ) {
-					foreach( $endpoint['args'] as $name => $args ) {
-						$synopsis[] = array(
-							'name'        => $name,
-							'type'        => 'assoc',
-							'description' => ! empty( $args['description'] ) ? $args['description'] : '',
-							'optional'    => empty( $args['required'] ) ? true : false,
-						);
-					}
-				}
-
-				if ( in_array( $command, array( 'list', 'get' ) ) ) {
-					$synopsis[] = array(
-						'name'        => 'fields',
-						'type'        => 'assoc',
-						'description' => 'Limit response to specific fields. Defaults to all fields.',
-						'optional'    => true,
-					);
-					$synopsis[] = array(
-						'name'        => 'format',
-						'type'        => 'assoc',
-						'description' => 'Limit response to specific fields. Defaults to all fields.',
-						'optional'    => true,
-					);
-				}
-
-				$methods = array(
-					'list'       => 'list_items',
-					'create'     => 'create_item',
-					'delete'     => 'delete_item',
-					'get'        => 'get_item',
-					'update'     => 'update_item',
+			$synopsis = array();
+			if ( in_array( $command, array( 'delete', 'get', 'update' ) ) ) {
+				$synopsis[] = array(
+					'name'        => 'id',
+					'type'        => 'positional',
+					'description' => 'The id for the resource.',
+					'optional'    => false,
 				);
-
-				WP_CLI::add_command( "{$parent} {$command}", array( $rest_command, $methods[ $command ] ), array(
-					'synopsis' => $synopsis,
-				) );
-
 			}
 
+			if ( ! empty( $endpoint['args'] ) ) {
+				foreach( $endpoint['args'] as $name => $args ) {
+					$synopsis[] = array(
+						'name'        => $name,
+						'type'        => 'assoc',
+						'description' => ! empty( $args['description'] ) ? $args['description'] : '',
+						'optional'    => empty( $args['required'] ) ? true : false,
+					);
+				}
+			}
+
+			if ( in_array( $command, array( 'list', 'get' ) ) ) {
+				$synopsis[] = array(
+					'name'        => 'fields',
+					'type'        => 'assoc',
+					'description' => 'Limit response to specific fields. Defaults to all fields.',
+					'optional'    => true,
+				);
+				$synopsis[] = array(
+					'name'        => 'format',
+					'type'        => 'assoc',
+					'description' => 'Limit response to specific fields. Defaults to all fields.',
+					'optional'    => true,
+				);
+			}
+
+			$methods = array(
+				'list'       => 'list_items',
+				'create'     => 'create_item',
+				'delete'     => 'delete_item',
+				'get'        => 'get_item',
+				'update'     => 'update_item',
+			);
+
+			WP_CLI::add_command( "{$parent} {$command}", array( $rest_command, $methods[ $command ] ), array(
+				'synopsis' => $synopsis,
+				'when'     => ! empty( $command_args['when'] ) ? $command_args['when'] : '',
+			) );
 		}
 	}
 	
--- wp-rest-cli.php
+++ wp-rest-cli.php
@@ -6,6 +6,6 @@
 require_once __DIR__ . '/inc/RestCommand.php';
 require_once __DIR__ . '/inc/Runner.php';
 
-WP_CLI::add_hook( 'before_wp_load', '\WP_REST_CLI\Runner::before_wp_load' );
+\WP_REST_CLI\Runner::load_remote_commands();
 WP_CLI::add_hook( 'after_wp_load', '\WP_REST_CLI\Runner::after_wp_load' );
 
