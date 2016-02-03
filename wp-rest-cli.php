a941ff0fccbca7072da6c93ca8d9880ddce350a4
Daniel Bachhuber <daniel@handbuilt.co>
2016-02-03 14:38:10 -0800

    Wrap in a conditional so test suite doesn't load it.

--- wp-rest-cli.php
+++ wp-rest-cli.php
@@ -6,6 +6,7 @@
 require_once __DIR__ . '/inc/RestCommand.php';
 require_once __DIR__ . '/inc/Runner.php';
 
-\WP_REST_CLI\Runner::load_remote_commands();
-WP_CLI::add_hook( 'after_wp_load', '\WP_REST_CLI\Runner::after_wp_load' );
-
+if ( class_exists( 'WP_CLI' ) ) {
+	\WP_REST_CLI\Runner::load_remote_commands();
+	WP_CLI::add_hook( 'after_wp_load', '\WP_REST_CLI\Runner::after_wp_load' );
+}
