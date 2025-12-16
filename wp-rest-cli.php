dedd3bbb7ef54dfc6143494d2fed4b46c8da4fbe
Pascal Birchler <pascalb@google.com>
2025-12-16 16:26:35 +0100

    Do manual require again

--- wp-rest-cli.php
+++ wp-rest-cli.php
@@ -7,11 +7,8 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$wp_rest_cli_autoloader = __DIR__ . '/vendor/autoload.php';
-
-if ( file_exists( $wp_rest_cli_autoloader ) ) {
-	require_once $wp_rest_cli_autoloader;
-}
+require_once __DIR__ . '/inc/RestCommand.php';
+require_once __DIR__ . '/inc/Runner.php';
 
 WP_REST_CLI\Runner::load_remote_commands();
 WP_CLI::add_hook( 'after_wp_load', [ WP_REST_CLI\Runner::class, 'after_wp_load' ] );
