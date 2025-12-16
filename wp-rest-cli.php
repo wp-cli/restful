fef409676d43d8a4a9bf8bc72befe313779ee3f1
Pascal Birchler <pascalb@google.com>
2025-12-16 16:15:40 +0100

    More tweaks

--- wp-rest-cli.php
+++ wp-rest-cli.php
@@ -3,11 +3,7 @@
  * Use WP-API at the command line.
  */
 
-namespace WP_REST_CLI;
-
-use WP_CLI;
-
-if ( ! class_exists( '\WP_CLI' ) ) {
+if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
@@ -17,5 +13,5 @@ if ( file_exists( $wp_rest_cli_autoloader ) ) {
 	require_once $wp_rest_cli_autoloader;
 }
 
-Runner::load_remote_commands();
-WP_CLI::add_hook( 'after_wp_load', [ Runner::class, 'after_wp_load' ] );
+WP_REST_CLI\Runner::load_remote_commands();
+WP_CLI::add_hook( 'after_wp_load', [ WP_REST_CLI\Runner::class, 'after_wp_load' ] );
